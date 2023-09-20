953c44090c1d814be6b27688cf7801f136fdb17e
Kristiyan Ivanov <kris.ivanov97@gmail.com>
2023-09-20 17:46:41 +0300

    Folder rename

--- first_steps_in_coding_lab/07_projects_creation.py
+++
@@ -1,8 +0,0 @@
-Hours_Per_Project = 3
-
-architecture_name = input()
-projects = int(input())
-
-hours_needed = projects * Hours_Per_Project
-
-print(f"The architect {architecture_name} will need {hours_needed} hours to complete {projects} project/s.")
